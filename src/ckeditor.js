1bdc4174565353c10f85bc1914f5db469901eb97
Suresh918 <suresh918.uputhula@gmail.com>
2019-09-03 13:19:04 +0530

    strikethrough plugin added
    strikethrough fucntionality is a mostly used plugin in the toolbar. These changes includes that plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,6 +11,7 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -37,6 +38,7 @@ BalloonEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
+	Strikethrough,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -63,6 +65,7 @@ BalloonEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+			'strikethrough',
 			'link',
 			'bulletedList',
 			'numberedList',
